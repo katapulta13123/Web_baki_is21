d96b22704c2a7b3d489e764ada20153e8461c3f9
MuRchi4me <MuRchi4me@gmail.com>
2023-12-23 00:10:23 +0700

    Добавил лр6

---
+++ students/Kostin Egor/Lr6/Lr6/modules/consts.js
@@ -0,0 +1,5 @@
+export const groupId = 223652294
+export const accessToken = 'vk1.a.wCUFqT_tt-BQ9CjSKXPPDfoPDJnUYESPhfJ_bcN8nN2Op2Z3PL-mOflE_KR9fBcLzOEkJlj7f5Qia2xOwUyXBzSG07gJzFeMDUylsxntkY3EuusnJr3MVsAVftEoF2-AJkUqQliv60oR-JOTmtCPnj3dKEtVhgdti__4rRbSpvVlHQVhmoIXedJw8NXalF43HMXhQkaTqwsRiSABRjZEKg'
+export const version = '5.131'
+export const peerId2 = 2000000002
+export const peerId1 = 2000000001
