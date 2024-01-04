ed9a27e0a46fde6891c090ed8f13844c7da5f9e3
Посметюк Антон <112932091+Subarashi-Alcoholic@users.noreply.github.com>
2024-01-04 13:54:25 +0700

    Лабораторная работа №4 и №5

---
+++ students/Posmetyuk_Anton/Lr3/modules/consts.js
@@ -0,0 +1,5 @@
+export const groupId = 224111809
+export const accessToken = 'vk1.a.NvXBoTny7AkiexjCIylGdLmxjYynePYyIcEymroiGlcmD1EXiKAmvuQjxDcMW6pQsqr8Y5-dhEgs9C-fjO3gnT7pHkV-RbpbZFOQBRvATWNDA67j8ogkMpZ2ytjllaG9XD5XS_sA5bI5yYvT5LMLPBjt095aHkmgoEw0qcJK6eA5pyYVegY853bTMol4aAIqcPBFsUGkZLkHHOH-X8Lfxg'
+export const version = '5.199'
+export const peerId2 = 2000000002
+export const peerId1 = 2000000001
